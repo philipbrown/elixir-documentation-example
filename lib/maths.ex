4e4db1c94c07c7b9151b0edf564b4bf33864da17
Philip Brown <phil@ipbrown.com>
2016-10-05 18:28:47 +0100

    Add module documentation

--- lib/maths.ex
+++ lib/maths.ex
@@ -1,6 +1,11 @@
 defmodule Maths do
+  @moduledoc """
+  A module that implements functions for performing simple
+  mathematic calculations
+  """
+
   def add(left, right) do
     # Add two numbers together
     left + right
-  en
+  end
 end
