9592cfd8154e1294deb26146e364430899b53d2a
Philip Brown <phil@ipbrown.com>
2016-10-05 18:46:59 +0100

    Generate documentation

--- mix.exs
+++ mix.exs
@@ -27,6 +27,7 @@ defmodule Maths.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:earmark, "~> 0.1", only: :dev},
+    {:ex_doc, "~> 0.11", only: :dev}]
   end
 end
